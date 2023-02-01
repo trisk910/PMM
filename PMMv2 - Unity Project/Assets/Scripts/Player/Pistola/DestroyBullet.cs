3bfdf1d2d3c06101b245705cbcc17273d069e4ec
Marc Bernabeu Rodriguez <marc_b_r@hotmail.com>
2023-02-02 00:57:53 +0100

    Texturas 2n Piso, Assets 2n piso a medias

---
+++ PMMv2 - Unity Project/Assets/Scripts/Player/Pistola/DestroyBullet.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBullet : MonoBehaviour
+{
+    public void caseDestroyBullet()
+    {
+        Destroy(this.gameObject);
+    }
+}
